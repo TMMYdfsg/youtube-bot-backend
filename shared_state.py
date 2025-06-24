458abc9237d73548bdcddca416b971b9d681eb31
TMMYdfsg <haruka_tomoya_714_810_1226@yahoo.co.jp>
2025-06-24 23:01:20 +0900

    Update shared_state.py

--- shared_state.py
+++ shared_state.py
@@ -1,6 +1,6 @@
 # shared_state.py
 # アプリケーション全体で共有したい情報をここに保存します
 
-CURRENT_LIVE_CHAT_ID = None
+CURRENT_LIVE_CHAT_ID: Optional[str] = None
+CURRENT_VIDEO_ID: Optional[str] = None
 YOUTUBE_SERVICE = None
-CURRENT_VIDEO_ID = None
